ff331fbb88d3e13866f17a72ea39698f241308e9
Hacktix <zecmad16@htlkaindorf.at>
2021-03-27 16:01:56 +0100

    Implemented call u16 instruction

--- js/gb/instr/instrs.js
+++ js/gb/instr/instrs.js
@@ -27,6 +27,40 @@ var funcmap = {
         }
     },
 
+    0xcd: (cycle)=>{
+        switch(cycle) {
+            default:
+                console.log(registers.pc.toString(16))
+                nextfunc = funcmap[0xcd].bind(this, 1);
+                break;
+            case 1:
+                tmp.push(readByte(registers.pc++));
+                nextfunc = funcmap[0xcd].bind(this, 2);
+                console.log(`  CALL u16 | read u16:lower`);
+                break;
+            case 2:
+                tmp.push(tmp.pop() | (readByte(registers.pc++) << 8));
+                nextfunc = funcmap[0xcd].bind(this, 3);
+                console.log(`  CALL u16 | read u16:upper`);
+                break;
+            case 3:
+                nextfunc = funcmap[0xcd].bind(this, 4);
+                console.log(`  CALL u16 | branch decision?`);
+                break;
+            case 4:
+                writeByte(--registers.sp, (registers.pc & 0xff00) >> 8);
+                nextfunc = funcmap[0xcd].bind(this, 5);
+                console.log(`  CALL u16 | write pc:upper->(--sp)`);
+                break;
+            case 5:
+                writeByte(--registers.sp, registers.pc & 0xff);
+                registers.pc = tmp.pop();
+                nextfunc = fetchInstruction;
+                console.log(`  CALL u16 | write pc:upper->(--sp)`);
+                break;
+        }
+    },
+
     // DI
     0xf3: () => {
         intr_state.ime = false;
