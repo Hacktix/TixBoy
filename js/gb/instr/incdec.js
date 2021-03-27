7466b22fc0e82d3bc7ee9d2f63218ac0f4261a1f
Hacktix <zecmad16@htlkaindorf.at>
2021-03-27 15:36:08 +0100

    Implemented dec, r8 instructions

--- js/gb/instr/incdec.js
+++ js/gb/instr/incdec.js
@@ -9,7 +9,7 @@ function _inc_mem_hl(cycle) {
         case 1:
             tmp.push(readByte(registers.hl));
             nextfunc = _inc_mem_hl.bind(this, 2);
-            console.log(`  INC (hl) | read (hl)`);
+            //console.log(`  INC (hl) | read (hl)`);
         case 2:
             registers.flag_h = (tmp[0] & 0xf) === 0xf;
             let v = (tmp.pop() + 1) & 0xff;
@@ -17,7 +17,7 @@ function _inc_mem_hl(cycle) {
             registers.flag_n = false;
             writeByte(registers.hl, v);
             nextfunc = fetchInstruction
-            console.log(`  INC (hl) | write (hl)`);
+            //console.log(`  INC (hl) | write (hl)`);
     }
 }
 for(let i = 0x04; i <= 0x3c; i += 0x08) {
@@ -25,12 +25,49 @@ for(let i = 0x04; i <= 0x3c; i += 0x08) {
         funcmap[i] = _inc_mem_hl.bind(this);
     else
         funcmap[i] = ((target) => {
-            registers.flag_h = (tmp[0] & 0xf) === 0xf;
+            registers.flag_h = (registers[target] & 0xf) === 0xf;
             let v = (registers[target] + 1) & 0xff;
             registers.flag_z = v === 0;
             registers.flag_n = false;
             registers[target] = v;
             nextfunc = fetchInstruction;
-            console.log(`  INC ${target}`);
+            //console.log(`  INC ${target}`);
+        }).bind(this, ["b", "c", "d", "e", "h", "l", "(hl)", "a"][(i & 0b111000) >> 3])
+}
+
+//-------------------------------------------------------------------------------
+// DEC r8
+//-------------------------------------------------------------------------------
+function _dec_mem_hl(cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = _dec_mem_hl.bind(this, 1);
+            break;
+        case 1:
+            tmp.push(readByte(registers.hl));
+            nextfunc = _dec_mem_hl.bind(this, 2);
+            //console.log(`  DEC (hl) | read (hl)`);
+        case 2:
+            registers.flag_h = (tmp[0] & 0xf) === 0;
+            let v = (tmp.pop() - 1) & 0xff;
+            registers.flag_z = v === 0;
+            registers.flag_n = true;
+            writeByte(registers.hl, v);
+            nextfunc = fetchInstruction
+            //console.log(`  DEC (hl) | write (hl)`);
+    }
+}
+for(let i = 0x05; i <= 0x3d; i += 0x08) {
+    if(i === 0x35)
+        funcmap[i] = _dec_mem_hl.bind(this);
+    else
+        funcmap[i] = ((target) => {
+            registers.flag_h = (registers[target] & 0xf) === 0;
+            let v = (registers[target] - 1) & 0xff;
+            registers.flag_z = v === 0;
+            registers.flag_n = true;
+            registers[target] = v;
+            nextfunc = fetchInstruction;
+            //console.log(`  DEC ${target}`);
         }).bind(this, ["b", "c", "d", "e", "h", "l", "(hl)", "a"][(i & 0b111000) >> 3])
 }
