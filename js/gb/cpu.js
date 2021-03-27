d30bed284dd7159a23b229d016acc24a5f9177c3
Hacktix <zecmad16@htlkaindorf.at>
2021-03-27 22:38:42 +0100

    Added checkbox for debug log downloads

--- js/gb/cpu.js
+++ js/gb/cpu.js
@@ -2,6 +2,8 @@ const CLOCK_FREQ = 4194304;
 const BLOCK_SIZE = 5000;
 var CYCLE_COUNT = 0;
 
+const DEBUG_LOG_CHECKBOX = document.getElementById("logdownload");
+
 // Include instruction mappings
 include('gb/instr/instrs.js');
 
@@ -117,8 +119,9 @@ var dbg_log = "";
 function fetchInstruction() {
 
     // Debug Logging
-    dbg_log += `A: ${registers.a.toString(16).padStart(2, '0')} F: ${registers.f.toString(16).padStart(2, '0')} B: ${registers.b.toString(16).padStart(2, '0')} C: ${registers.c.toString(16).padStart(2, '0')} D: ${registers.d.toString(16).padStart(2, '0')} E: ${registers.e.toString(16).padStart(2, '0')} H: ${registers.h.toString(16).padStart(2, '0')} L: ${registers.l.toString(16).padStart(2, '0')} SP: ${registers.sp.toString(16).padStart(4, '0')} PC: 00:${registers.pc.toString(16).padStart(4, '0')} (${readByte(registers.pc).toString(16).padStart(2, '0')} ${readByte(registers.pc+1).toString(16).padStart(2, '0')} ${readByte(registers.pc+2).toString(16).padStart(2, '0')} ${readByte(registers.pc+3).toString(16).padStart(2, '0')})\n`.toUpperCase();
-    
+    if(DEBUG_LOG_CHECKBOX.checked)
+        dbg_log += `A: ${registers.a.toString(16).padStart(2, '0')} F: ${registers.f.toString(16).padStart(2, '0')} B: ${registers.b.toString(16).padStart(2, '0')} C: ${registers.c.toString(16).padStart(2, '0')} D: ${registers.d.toString(16).padStart(2, '0')} E: ${registers.e.toString(16).padStart(2, '0')} H: ${registers.h.toString(16).padStart(2, '0')} L: ${registers.l.toString(16).padStart(2, '0')} SP: ${registers.sp.toString(16).padStart(4, '0')} PC: 00:${registers.pc.toString(16).padStart(4, '0')} (${readByte(registers.pc).toString(16).padStart(2, '0')} ${readByte(registers.pc+1).toString(16).padStart(2, '0')} ${readByte(registers.pc+2).toString(16).padStart(2, '0')} ${readByte(registers.pc+3).toString(16).padStart(2, '0')})\n`.toUpperCase();
+
     if(debug_brk.includes(registers.pc) && intervalId !== null)
         throw `Breakpoint hit: $${registers.pc.toString(16).padStart(4, '0')}`;
     let opcode = readByte(registers.pc++);
@@ -141,7 +144,7 @@ function execBlock() {
     } catch(e) {
         clearInterval(intervalId);
         intervalId = null;
-        download("log.txt", dbg_log);
+        if(DEBUG_LOG_CHECKBOX.checked) download("log.txt", dbg_log);
         console.error(e);
     }
 }
