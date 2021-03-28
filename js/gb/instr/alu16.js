1ce8ff522734e41f38124e98f6af644156569b76
Hacktix <zecmad16@htlkaindorf.at>
2021-03-28 01:51:58 +0100

    Implemented SP arithmetic instructions

--- js/gb/instr/alu16.js
+++ js/gb/instr/alu16.js
@@ -15,3 +15,56 @@ function _add_hl_r16(source, cycle) {
 }
 for(let i = 0x09; i <= 0x39; i += 0x10)
     funcmap[i] = _add_hl_r16.bind(this, ["bc", "de", "hl", "sp"][(i&0b110000) >> 4]);
+
+
+
+//-------------------------------------------------------------------------------
+// ADD sp, e8
+//-------------------------------------------------------------------------------
+function _add_sp_e8(cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = _add_sp_e8.bind(this, 1);
+            break;
+        case 1:
+            tmp.push(e8(readByte(registers.pc++)));
+            nextfunc = _add_sp_e8.bind(this, 2);
+            break;
+        case 2:
+            nextfunc = _add_sp_e8.bind(this, 3);
+            break;
+        case 3:
+            registers.flag_z = registers.flag_n = false;
+            registers.flag_h = (tmp[0] & 0xf) + (registers.sp & 0xf) > 0xf;
+            registers.flag_c = (tmp[0] & 0xff) + (registers.sp & 0xff) > 0xff;
+            registers.sp += tmp.pop();
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+funcmap[0xe8] = _add_sp_e8;
+
+
+
+//-------------------------------------------------------------------------------
+// LD HL, SP+e8
+//-------------------------------------------------------------------------------
+function _ld_hl_sp_e8(cycle) {
+    switch(cycle) {
+        default:
+            nextfunc = _ld_hl_sp_e8.bind(this, 1);
+            break;
+        case 1:
+            tmp.push(e8(readByte(registers.pc++)));
+            nextfunc = _ld_hl_sp_e8.bind(this, 2);
+            break;
+        case 2:
+            registers.flag_z = registers.flag_n = false;
+            registers.flag_h = (tmp[0] & 0xf) + (registers.sp & 0xf) > 0xf;
+            registers.flag_c = (tmp[0] & 0xff) + (registers.sp & 0xff) > 0xff;
+            registers.hl = registers.sp + tmp.pop();
+            nextfunc = fetchInstruction;
+            break;
+    }
+}
+funcmap[0xf8] = _ld_hl_sp_e8;
