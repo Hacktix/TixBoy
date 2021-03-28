f0840dd29f05e6919cbb136212f472e87b1fc7ca
Hacktix <zecmad16@htlkaindorf.at>
2021-03-28 03:36:36 +0200

    Implemented rla, rrca and rlca instructions

--- js/gb/instr/bitops.js
+++ js/gb/instr/bitops.js
@@ -6,7 +6,36 @@ funcmap[0x1f] = () => {
     registers.flag_c = (registers.a & 1) > 0;
     registers.a = (registers.a >> 1) | rv;
     registers.flag_h = registers.flag_n = registers.flag_z = false;
-    //console.log(`  RRA`);
+};
+
+//-------------------------------------------------------------------------------
+// RLA
+//-------------------------------------------------------------------------------
+funcmap[0x17] = () => {
+    let rv = registers.flag_c ? 1 : 0;
+    registers.flag_c = (registers.a & 0x80) > 0;
+    registers.a = (registers.a << 1) | rv;
+    registers.flag_h = registers.flag_n = registers.flag_z = false;
+};
+
+//-------------------------------------------------------------------------------
+// RRCA
+//-------------------------------------------------------------------------------
+funcmap[0x0f] = () => {
+    let rv = (registers.a & 1) << 7
+    registers.flag_c = (registers.a & 1) > 0;
+    registers.a = (registers.a >> 1) | rv;
+    registers.flag_h = registers.flag_n = registers.flag_z = false;
+};
+
+//-------------------------------------------------------------------------------
+// RLCA
+//-------------------------------------------------------------------------------
+funcmap[0x07] = () => {
+    let rv = (registers.a & 0x80) >> 7
+    registers.flag_c = (registers.a & 0x80) > 0;
+    registers.a = (registers.a << 1) | rv;
+    registers.flag_h = registers.flag_n = registers.flag_z = false;
 };
 
 //-------------------------------------------------------------------------------
