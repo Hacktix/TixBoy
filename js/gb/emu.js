7b3a5f25e7d845fa0387ff46523c5dcfe3f1c926
Hacktix <zecmad16@htlkaindorf.at>
2021-03-30 21:18:52 +0200

    Added fancy new frontend

--- js/gb/emu.js
+++ js/gb/emu.js
@@ -3,8 +3,9 @@ include('gb/cpu.js');
 include('gb/mem.js');
 
 // Initialize Canvas
-const lcd = document.getElementById('lcd').getContext('2d');
+const lcd = document.getElementById('lcd').getContext('2d', {alpha: false});
 lcd.scale(4, 4);
+lcd.fillStyle = 'rgb(156, 160, 76)';
 lcd.fillRect(0, 0, 160, 144);
 
 // Initialize ROM Select Listener
@@ -15,6 +16,8 @@ document.getElementById('rom').addEventListener('change', (e) => {
     fr.addEventListener('load', (e) => {
         loadRom(new Uint8Array(e.target.result));
         startCPU();
+        document.getElementsByTagName("body")[0].style.backgroundImage = "url('bgon.png')";
+        console.log(e)
     });
     fr.readAsArrayBuffer(e.target.files[0]);
 });
